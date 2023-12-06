a84ab24eb939f86546f0dc2e95d41d369cc6fb87
unknown <heroglasses155@gmail.com>
2023-12-06 13:19:22 +0630

    qrcode jpg downloader

--- src/components/QRGenerator.jsx
+++ src/components/QRGenerator.jsx
@@ -1,29 +1,78 @@
+import { Button, Input } from "antd";
 import React, { useState } from "react";
-import QrReader from "react-qr-reader";
+import QRCode from "qrcode.react";
 
 const QRGenerator = () => {
-  const [scannedText, setScannedText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [qr, setQr] = useState("");
 
-  const handleOnScan = (data) => {
-    if (data) {
-      setScannedText(data);
-    }
+  const handleTextToQR = () => {
+    // Handle text input and set QR code value
+    setQr(inputText);
   };
 
-  const handleOnError = (err) => {
-    console.error(err);
+  const downloadQRCode = () => {
+    const qrCodeURL = document
+      .getElementById("qrCodeEl")
+      .toDataURL("image/jpg")
+      .replace("image/jpg", "image/octet-stream");
+    console.log(qrCodeURL);
+    let aEl = document.createElement("a");
+    aEl.href = qrCodeURL;
+    aEl.download = "QR_Code.jpg";
+    document.body.appendChild(aEl);
+    aEl.click();
+    document.body.removeChild(aEl);
   };
 
   return (
-    <div style={{ width: "300px" }}>
-      <QrReader
-        delay={300}
-        onError={handleOnError}
-        onScan={handleOnScan}
-        legacyMode={false}
-        facingMode="user"
-      />
-      {scannedText && <p>Scanned Text: {scannedText}</p>}
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: "10px",
+        }}
+      >
+        <Input
+          style={{
+            border: "1px solid green",
+            borderRadius: "1px",
+            width: "300px",
+            marginRight: "20px",
+          }}
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          placeholder="Enter text for QR code"
+        />
+        <Button
+          onClick={handleTextToQR}
+          style={{
+            backgroundColor: "green",
+            color: "white",
+            borderRadius: "1px",
+          }}
+        >
+          Generate
+        </Button>
+      </div>
+      <div
+        style={{
+          marginTop: "50px",
+          marginLeft: "50px",
+        }}
+      >
+        {qr && (
+          <div>
+            <QRCode id="qrCodeEl" value={qr} />
+            <br />
+            <br />
+
+            <Button onClick={downloadQRCode}>Download QR Code</Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
